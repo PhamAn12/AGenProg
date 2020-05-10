d1898e94ea089273ac6b97b347d8eff5029716ae
PhamAn12 <phamkhacan1903@gmail.com>
2020-05-10 23:41:10 +0700

    update something

---
+++ src/main/AutoGenerateFolder/Buggy2/MidFuction2.java
@@ -0,0 +1,24 @@
+package Buggy2;
+
+public class MidFuction2 {
+    public int findMid(int a, int b) {
+        int r;
+        if(a == 0) {
+            System.out.print(b);
+            r = b;
+            r = r + 1;
+            return r;
+        }
+        while(b != 0) {
+            if (a > b) {
+                a = a - b;
+            }
+            else {
+                b = b - a;
+            }
+        }
+        System.out.print(b);
+        r = a;
+        return r;
+    }
+}
